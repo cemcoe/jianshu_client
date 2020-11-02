2aaab9bdb5fc83898bdc21333613448691c8c191
cemcoe <chemxcode@gmail.com>
2020-11-03 06:21:17 +0800

    fix: vue3废弃了slot

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view/>
+  <router-view />
+  <main-tab-bar
+    v-if="$route.meta.showFooter"
+    class="main-tab-bar"
+  ></main-tab-bar>
 </template>
+<script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+export default {
+  components: {
+    MainTabBar,
+  },
+};
+</script>
 
 <style>
 </style>
