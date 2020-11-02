c17476bdd39782e8db179e838e0c4a2abd8c5363
cemcoe <chemxcode@gmail.com>
2020-11-03 07:43:55 +0800

    refactor: HomeRecommend

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,10 @@
 <template>
-  <div>home</div>
+  <div>header</div>
   <router-view></router-view>
 </template>
 
 <script>
-import { getHomePostList } from "network/post.js";
-export default {
-  async setup() {
-    const data = await getHomePostList();
-    console.log(data);
-  },
-};
+export default {};
 </script>
 
 <style>
--- src/views/Home/childComps/HomeRecommend/HomeRecommend.vue
+++ src/views/Home/childComps/HomeRecommend/HomeRecommend.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    recommend
+    {{ state.postList }}
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
+import { getHomePostList } from "network/post";
+
 export default {
+  setup() {
+    const state = reactive({
+      postList: [],
+      currentPage: 1,
+      perPage: 10,
+      isShowAlert: true,
+      alertText: `
+      本项目是一个移动端项目，使用视口在 500px 以内的设备访问以获得更好的访问体验。
+      再次提醒，
+      网站文章数据由狗屁不通文章生成器生成，
+      不具有任何的价值，
+      仅供占位测试使用，亦不代表本人观点。
+      `,
+    });
 
-}
+    async function getData() {
+      state.postList = await getHomePostList();
+    }
+
+    getData();
+
+    return {
+      state,
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
