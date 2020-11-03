eed61497ee6dcdc6229b47b355fe6a7987eb11e5
cemcoe <chemxcode@gmail.com>
2020-11-03 11:57:18 +0800

    refactor: hometabcontrol

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import './assets/css/reset.css'
+import './assets/css/values.css'
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <home-header></home-header>
+  <home-tab-control></home-tab-control>
   <router-view></router-view>
 </template>
 
 <script>
 import HomeHeader from "./childComps/HomeHeader/HomeHeader";
+import HomeTabControl from "./childComps/HomeTabControl/HomeTabControl";
 
 export default {
   components: {
     HomeHeader,
+    HomeTabControl,
   },
 };
 </script>
